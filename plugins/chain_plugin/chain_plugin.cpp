3506326e8e79439baae11aab729965c47199f617
SeanChen <aws4amazon@163.com>
2022-01-22 16:19:00 +0800

    rename

--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -631,9 +631,9 @@ void chain_plugin::plugin_initialize(const variables_map& options) {
 
    try {
       try {
-         genesis_state gs; // Check if EOSIO_ROOT_KEY is bad
+         genesis_state gs; // Check if AMAX_ROOT_KEY is bad
       } catch ( const fc::exception& ) {
-         elog( "EOSIO_ROOT_KEY ('${root_key}') is invalid. Recompile with a valid public key.",
+         elog( "AMAX_ROOT_KEY ('${root_key}') is invalid. Recompile with a valid public key.",
                ("root_key", genesis_state::eosio_root_key));
          throw;
       }
