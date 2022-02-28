272bc81aa7247b58634b8be7007841fea2ada08f
SeanChen <aws4amazon@163.com>
2022-02-28 15:12:36 +0800

    fix issue of error_code=8000000000000000000

--- unittests/test-contracts/payloadless/payloadless.cpp
+++ unittests/test-contracts/payloadless/payloadless.cpp
@@ -5,3 +5,6 @@ using namespace eosio;
 void payloadless::doit() {
    print("Im a payloadless action");
 }
+
+// use the EOSIO_DISPATCH() to avoid error code "8000000000000000000" 
+EOSIO_DISPATCH(payloadless, (doit) )
