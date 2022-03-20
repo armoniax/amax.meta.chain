25432d33d3fd70cb69d28c759eaf50c3c60cd2ec
metamaster2021 <aws4amazon@163.com>
2022-03-20 12:25:54 +0800

    improve block_timestamp_tests

--- unittests/block_timestamp_tests.cpp
+++ unittests/block_timestamp_tests.cpp
@@ -17,17 +17,22 @@ BOOST_AUTO_TEST_CASE(constructor_test) {
         
 	fc::time_point t(fc::seconds(978307200));	
 	block_timestamp_type bt2(t);
-	BOOST_TEST( bt2.slot == (978307200u * 1000u - 946684800u * 1000u) / config::block_interval_ms, "Time point constructor gives wrong value");
+
+	BOOST_TEST( bt2.slot == 
+		(t.time_since_epoch().count() / 1000 - config::block_timestamp_epoch) / config::block_interval_ms, 
+		"Time point constructor gives wrong value");
 }
 
 BOOST_AUTO_TEST_CASE(conversion_test) {
 	block_timestamp_type bt;
 	fc::time_point t = (fc::time_point)bt;
-	BOOST_TEST(t.time_since_epoch().to_seconds() == 946684800ll, "Time point conversion failed");
+	BOOST_TEST(t.time_since_epoch().to_seconds() == config::block_timestamp_epoch / 1000, "Time point conversion failed");
 
 	block_timestamp_type bt1(200);
 	t = (fc::time_point)bt1;
-	BOOST_TEST(t.time_since_epoch().to_seconds() == 946684900ll, "Time point conversion failed");
+	BOOST_TEST(t.time_since_epoch().to_seconds() == 
+		(config::block_timestamp_epoch + 200 * config::block_interval_ms) / 1000, 
+		"Time point conversion failed");
 
 }
 
