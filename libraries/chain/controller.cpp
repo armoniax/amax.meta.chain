520f701bcae96016bf791e54db7f1bd8563a938a
metamaster2021 <aws4amazon@163.com>
2024-10-14 19:38:55 +0800

    refine fetch_block_by_id

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -3142,17 +3142,15 @@ const global_property_object& controller::get_global_properties()const {
 signed_block_ptr controller::fetch_block_by_id( block_id_type id )const {
    auto state = my->fork_db.get_block(id);
    if( state && state->block ) return state->block;
-   auto bptr = fetch_block_by_number( block_header::num_from_id(id) );
+
+   auto block_num = block_header::num_from_id(id);
+   auto bptr = fetch_block_by_number( block_num );
    if( bptr && bptr->id() == id ){
       return bptr;
    }else{
       bptr = fetch_block_by_number( block_header::num_from_id(id) , true );
-      fc_dlog(_backup_block_trace_log,"[BACKUP_TRACE] fetch backup block by id in controller, find: ${found}",("found",bptr != nullptr? true: false));
-      if( bptr ){
-         //fc_dlog(_backup_block_trace_log,"[BACKUP_TRACE] found id: ${fid}: wanted: ${wid}",("fid",bptr->id())("wid",id));
-         if( bptr->id() == id ){
-            return bptr;
-         }
+      if( bptr && bptr->id() == id ){
+         return bptr;
       }
    }
    return signed_block_ptr();
@@ -3175,6 +3173,7 @@ signed_block_ptr controller::fetch_block_by_number( uint32_t block_num , bool is
          return fb->backup_block;
       }
    }
+   return signed_block_ptr();
 } FC_CAPTURE_AND_RETHROW( (block_num) ) }
 
 block_state_ptr controller::fetch_block_state_by_id( block_id_type id )const {
